ae347402d541f01a3b4b40b42d05e8847215ece3
Ryann Lu <lu.ryann.2n@tamu.edu>
2024-10-09 20:44:22 -0500

    whatever, just arrows then

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,9 @@ module ApplicationHelper
     def sort_indicator(column)
         if params[:sort_by] == column
             if params[:order] == 'asc'
-                content_tag(:span, "↑", class: "sort-indicator").html_safe
+                " ↑"
             else
-                content_tag(:span, "↓", class: "sort-indicator").html_safe
+                " ↓"
             end
         else
             ""
