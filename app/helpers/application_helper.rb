ec3c0f1574d7ac6a3b700f06da5f80a67776dbe1
Ryann Lu <lu.ryann.2n@tamu.edu>
2024-10-09 20:17:07 -0500

    testing styles and arrow indicator

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+    def sort_indicator(column)
+        if params[:sort_by] == column
+            if params[:order] == 'asc'
+                content_tag(:span, "↑", class: "sort-indicator up")
+            else
+                content_tag(:span, "↓", class: "sort-indicator down")
+            end
+        else
+            ""
+        end
+    end
 end
