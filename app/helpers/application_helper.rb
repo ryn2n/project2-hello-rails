899a2ba87008b5ec630e9b949637c0940b3c5bfb
Ryann Lu <lu.ryann.2n@tamu.edu>
2024-10-09 20:25:35 -0500

    css method for arrows

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,7 @@
 module ApplicationHelper
     def sort_indicator(column)
         if params[:sort_by] == column
-            if params[:order] == 'asc'
-                content_tag(:span, "↑", class: "sort-indicator up")
-            else
-                content_tag(:span, "↓", class: "sort-indicator down")
-            end
+            content_tag(:span, "", class: "sort-indicator #{params[:order]}")
         else
             ""
         end
