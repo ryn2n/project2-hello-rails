83137f137bb91737439942227f8f3e3a6268c4ef
Ryann Lu <lu.ryann.2n@tamu.edu>
2024-10-09 20:39:55 -0500

    trying something simpler

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,9 @@ module ApplicationHelper
     def sort_indicator(column)
         if params[:sort_by] == column
             if params[:order] == 'asc'
-                content_tag(:span, "↑", class: "sort-indicator asc")
+                content_tag(:span, "↑", class: "sort-indicator")
             else
-                content_tag(:span, "↓", class: "sort-indicator desc")
+                content_tag(:span, "↓", class: "sort-indicator")
             end
         else
             ""
